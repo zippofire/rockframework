6adc74a06e0e91567c43506a6e51fc450b926479
lourival.sabino.junior <lourival.sabino.junior@a79beb34-43a6-11de-b466-9154fc769079>
2010-03-08 14:14:57 +0000

    testes do wicket;

--- test-projects/wicket-test/src/net/woodstoc/rockframework/test/wicket/Index.java
+++ test-projects/wicket-test/src/net/woodstoc/rockframework/test/wicket/Index.java
@@ -1,16 +1,14 @@
 package net.woodstoc.rockframework.test.wicket;
 
 import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 
 public class Index extends WebPage {
 
 	public Index() {
 		super();
-		this.add(new Label("msg", "Teste do Wicket"));
-
-		this.add(new BookmarkablePageLink("link", Ajax.class));
+		this.add(new BookmarkablePageLink("ajax", Ajax.class));
+		this.add(new BookmarkablePageLink("people", PeopleListPage.class));
 	}
 
 }
--- test-projects/wicket-test/src/net/woodstoc/rockframework/test/wicket/TestApplication.java
+++ test-projects/wicket-test/src/net/woodstoc/rockframework/test/wicket/TestApplication.java
@@ -2,6 +2,7 @@ package net.woodstoc.rockframework.test.wicket;
 
 import org.apache.wicket.Page;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.apache.wicket.util.lang.PackageName;
 
 public class TestApplication extends WebApplication {
 
@@ -12,6 +13,12 @@ public class TestApplication extends WebApplication {
 	@Override
 	public Class<? extends Page> getHomePage() {
 		return Index.class;
+	}
+
+	@Override
+	protected void init() {
+		super.init();
+		this.mount("/pages", PackageName.forPackage(Index.class.getPackage()));
 	}
 
 }
