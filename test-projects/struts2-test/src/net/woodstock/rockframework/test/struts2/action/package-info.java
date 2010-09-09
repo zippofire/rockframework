ad70b20516c87a5749df4e1373054ea6b3e3b49e
lourival.sabino.junior <lourival.sabino.junior@a79beb34-43a6-11de-b466-9154fc769079>
2010-09-09 17:22:57 +0000

    exclusao dos projetos migrados; configuracao do ignore dos projetos novos; exclusao de arquivos de configuracao antigos;

--- test-projects/struts2-test/src/net/woodstock/rockframework/test/struts2/action/package-info.java
+++
@@ -1,3 +0,0 @@
-@org.apache.struts2.convention.annotation.Namespace("/test")
-package net.woodstock.rockframework.test.struts2.action;
-
--- test-projects/struts2-test/web/index.jsp
+++
@@ -1,8 +0,0 @@
-<html>
-	<head>
-		<title>Teste Captcha</title>
-	</head>
-	<body>
-		XXXX
-	</body>
-</html>
