31f2b1bfbe5828aa93fc873b1a9f4eb08caa4488
lourival.sabino.junior <lourival.sabino.junior@gmail.com>
2011-11-02 13:25:05 +0000

    atualizacao do framework

---
+++ rockframework-domain/src/main/java/br/net/woodstock/rockframework/domain/jee/Repository.java
@@ -0,0 +1,17 @@
+package br.net.woodstock.rockframework.domain.jee;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.inject.Qualifier;
+
+@Qualifier
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ ElementType.TYPE })
+public @interface Repository {
+	//
+}
